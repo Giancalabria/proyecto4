300230804a23c6058ee27f1520a4f6fe70e7dfc0
Giancalabria <80368451+Giancalabria@users.noreply.github.com>
2021-06-25 16:14:50 -0300

    Added assets + components + Login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,34 @@
+import { useState } from "react"
+import { Input } from "../components/input/input"
+import styles from "./login.module.scss"
+import auriculares from "../assets/icons/auriculares.svg"
+import { Button } from "../components/button/button"
 export const Login = () => {
-    return <div>hola</div>
+    const [username, setUsernname] = useState()
+    const [password, setPassword] = useState()
+
+    const verify = () => {
+        let user = "gian"
+        let pass = "hola"
+        if (username === user && password === pass){
+            window.location.replace("http://localhost:3000/main");
+        }  
+    }
+
+    return( 
+    
+    <div className={styles.login}>
+        <div className={styles.left}>
+            <h1 className={styles.title}>Prize finder</h1>
+            <div className={styles.subtitleDiv}>
+                <img src={auriculares} className={styles.logo}/>
+                <p className={styles.subtitle}>Encuentra los mejores premios aquí!</p>
+            </div>
+            <Input label="Usuario" name="user" value={username} onChange={({target}) => setUsernname(target.value)} />
+            <Input label="Contraseña" name="password" value={password} onChange={({target}) => setPassword(target.value)} type="password"/>
+            <Button className="button" onClick={() => verify()} content="Iniciar sesion"/>
+        </div>
+        <div className={styles.right}>
+        </div>
+    </div>)
 }
